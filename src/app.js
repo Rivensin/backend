0e824090382f2bcbc539a2c9f4d064a15d197810
riven <rivensinn@gmail.com>
2026-08-03 23:18:34 +0700

    feat(all): change domain cors to url vercel app

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://watchlist-frontend.vercel.app",
+      "https://watchlistmovieapp.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
